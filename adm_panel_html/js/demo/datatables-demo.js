a48af52ee90e778fb44eb81468e3e072620b009a
Karolis Zabulis <karoliszabulis@gmail.com>
2025-03-09 10:34:33 +0200

    Primary design of admin page
    Using template from https://startbootstrap.com/theme/sb-admin-2
    
    Template is highly minimized and only some parts of it are used.

---
+++ adm_panel_html/js/demo/datatables-demo.js
@@ -0,0 +1,6 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#userDataTable').DataTable();
+  $('#visitsDataTable').DataTable();
+  $('#listingsDataTable').DataTable();
+});
---
+++ adm_panel_html/js/form-handler.js
@@ -0,0 +1,18 @@
+let userPopup = document.getElementById("userPopup")
+let listingPopup = document.getElementById ("listingPopup");
+
+function openUserPopup(){
+  userPopup.classList.add("open-popup");
+}
+
+function closeUserPopup(){
+  userPopup.classList.remove("open-popup");
+}
+
+function openListingPopup(){
+  listingPopup.classList.add("open-popup");
+}
+
+function closeListingPopup(){
+  listingPopup.classList.remove("open-popup");
+}
