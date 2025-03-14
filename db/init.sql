593be652c465ab38fe25d044cf54f92f4195c3be
Karolis Zabulis <karoliszabulis@gmail.com>
2025-03-14 14:59:39 +0200

    Insertion of primary data into db

--- db/init.sql
+++ db/init.sql
@@ -31,5 +31,22 @@ CREATE TABLE meeting (
     mId SERIAL NOT NULL,
     aId INT REFERENCES account(aId),
     date DATE NOT NULL,
+    time TIME NOT NULL,
     CONSTRAINT p_key PRIMARY KEY (mId, aId)
 );
+
+INSERT INTO privilege (pid, privilegeName) VALUES
+(default, 'User'),
+(default, 'Admin');
+
+-- This later will be seeded with laravel, only creating admin now
+INSERT INTO account (aid, pid, fname, lname, password, email) VALUES
+(default, 2, 'admin', 'admin', 'goodadmin', 'admin@admin.com');
+(default, 1, 'testuser', 'testuser', 'testuser', 'test@user.com');
+
+INSERT INTO carListing (cid, model, mileage, comments, makeyear, color, price, imgpath, manufacturer) VALUES 
+(default, 'SF90', 2198, 'Condition great', 2019, 'red', 250000, 'somepath', 'Ferrari'),
+(default, '812 Superfast', 2198, 'Condition mint', 2020, 'red', 750000, 'somepath', 'Ferrari');
+
+INSERT INTO meeting (mid, aid, date, time) VALUES
+(default, 2, '2025-04-16', '15:25');
